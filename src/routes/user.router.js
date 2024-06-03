13d344c2cbd15ecc6bcf7f8f6296e99b35d0c674
Christopher Radilla <gonzalez67061@gmail.com>
2024-06-02 19:58:14 -0600

    first commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,15 @@
+const { getAll, create, getOne, destroy, update } = require('../controllers/user.controllers');
+const express = require('express');
+
+const userRouter = express.Router();
+// rutas estaticas
+userRouter.route("/")
+		.get(getAll)
+        .post(create)
+        
+userRouter.route('/:id')
+        .get(getOne)
+        .delete(destroy)
+        .put(update)
+
+module.exports = userRouter;
